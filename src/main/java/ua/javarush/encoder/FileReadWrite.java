184f73e25b517a920e6be38b9b782f5c690796f2
Vladyslava Kanivets <kanivetsvlada21@gmail.com>
2023-08-21 16:56:22 +0300

    implemented defineLanguage

--- src/main/java/ua/javarush/encoder/FileReadWrite.java
+++ src/main/java/ua/javarush/encoder/FileReadWrite.java
@@ -9,17 +9,36 @@ public class FileReadWrite {
         this.source = source;
     }
 
-    public void useCryptography() {
+    public void useCryptography(Cipher cipher) {
 
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
              BufferedWriter writer = new BufferedWriter(new FileWriter(new CreateName().createNewName()))) {
 
             while (reader.ready()) {
-                writer.write(CaesarCipher.encryptIt(reader.read()));
+                writer.write(cipher.encryptIt(reader.read()));
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+    public String defineLanguage() {
+        int engCharCounter = 0;
+        int ukrCharCounter = 0;
+        try (FileReader fileReader = new FileReader(source)) {
+            char[] chars = new char[10];
+            int amountInBuffer = fileReader.read(chars);
+            for (int i = 0; i < amountInBuffer; i++) {
+                if (chars[i] >= (int) 'a' & chars[i] <= (int) 'z') {
+                    engCharCounter++;
+                } else if (chars[i] >= (int) 'а' & chars[i] <= (int) 'я') {
+                    ukrCharCounter++;
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return engCharCounter>ukrCharCounter?"eng":"ukr";
     }
 
     private class CreateName {
