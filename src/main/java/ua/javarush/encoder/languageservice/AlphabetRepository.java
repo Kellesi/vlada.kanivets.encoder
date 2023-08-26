2263ac7ad4aa7d968599b17a25ef9d06dedc1c4b
Vladyslava Kanivets <kanivetsvlada21@gmail.com>
2023-08-26 12:47:29 +0300

    + AlphabetRepository char frequency storage

--- src/main/java/ua/javarush/encoder/languageservice/AlphabetRepository.java
+++ src/main/java/ua/javarush/encoder/languageservice/AlphabetRepository.java
@@ -1,4 +1,4 @@
-package ua.javarush.encoder.LanguageService;
+package ua.javarush.encoder.languageservice;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,22 +13,30 @@ import java.util.List;
 
 public class AlphabetRepository {
     private static HashMap<String, List<Character>> languageToAlphabet = new HashMap<>();
+    private static HashMap<String, List<Character>> freq = new HashMap<>();
 
     static {
-        RepoFileService.readMap();
+        languageToAlphabet=RepoFileService.readMap(RepoFileService.repoFile);
+        freq=RepoFileService.readMap(RepoFileService.charFrequencyStat);
     }
 
     public static HashMap<String, List<Character>> getLangMap() {
         return new HashMap<>(languageToAlphabet);
     }
 
-    public static ArrayList<Character> getLang(String langCode) {
+    public static ArrayList<Character> getAlphabet(String langCode) {
         return new ArrayList<>(languageToAlphabet.get(langCode));
     }
-
+    public static ArrayList<Character> getCharFrequency(String langCode) {
+        return new ArrayList<>(freq.get(langCode));
+    }
+    public static void addFreq(String languageName, List<Character> languageCharacters) {
+        freq.put(languageName, languageCharacters);
+        RepoFileService.writeMap(RepoFileService.charFrequencyStat, freq);
+    }
     public static void addLanguage(String languageName, List<Character> languageCharacters) {
         languageToAlphabet.put(languageName, languageCharacters);
-        RepoFileService.writeMap();
+        RepoFileService.writeMap(RepoFileService.repoFile, languageToAlphabet);
     }
     public static void removeLanguage(String languageName){
         for (DefaultLanguages lang: DefaultLanguages.values()) {
@@ -37,7 +45,7 @@ public class AlphabetRepository {
             }
         }
         languageToAlphabet.remove(languageName);
-        RepoFileService.writeMap();
+        RepoFileService.writeMap(RepoFileService.repoFile, languageToAlphabet);
     }
     public static HashSet<String> availableLanguages(){
         return new HashSet<>(languageToAlphabet.keySet());
@@ -45,19 +53,22 @@ public class AlphabetRepository {
 
     private static class RepoFileService {
         private static final File repoFile = new File("src/main/resources/LangRepo.txt");
+        private static final File charFrequencyStat = new File("src/main/resources/charFrequencyStat.txt");
 
-        static HashMap<String, List<Character>> readMap() {
-            try (ObjectInputStream reader = new ObjectInputStream(new FileInputStream(repoFile))) {
-                languageToAlphabet = (HashMap<String, List<Character>>) reader.readObject();
+        static HashMap<String, List<Character>> readMap(File fileName) {
+            HashMap<String, List<Character>> map = new HashMap<>();
+            try (ObjectInputStream reader = new ObjectInputStream(new FileInputStream(fileName))) {
+                map = (HashMap<String, List<Character>>) reader.readObject();
             } catch (IOException | ClassNotFoundException ex) {
                 ex.printStackTrace();
             }
-            return languageToAlphabet;
+            return map;
         }
 
-        static void writeMap() {
-            try (ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(repoFile))) {
-                writer.writeObject(languageToAlphabet);
+        static void writeMap(File fileName, HashMap<String, List<Character>> mapToWrite) {
+            HashMap<String, List<Character>> map = new HashMap<>(mapToWrite);
+            try (ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(fileName))) {
+                writer.writeObject(map);
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
