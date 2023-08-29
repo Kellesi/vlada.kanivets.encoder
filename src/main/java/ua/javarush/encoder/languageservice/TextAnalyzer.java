f440143e7971e22cc44af9248ecf31ec7b83d702
Vladyslava Kanivets <kanivetsvlada21@gmail.com>
2023-08-29 21:01:47 +0300

    TextAnalyzer methods refactoring

--- src/main/java/ua/javarush/encoder/languageservice/TextAnalyzer.java
+++ src/main/java/ua/javarush/encoder/languageservice/TextAnalyzer.java
@@ -7,83 +7,78 @@ import java.util.List;
 import java.util.Map;
 
 public class TextAnalyzer {
-    public static String getLanguage(Integer[] buffer) {
+    private final String[] buffer;
+
+    public TextAnalyzer(String[] buffer) {
+        this.buffer = buffer;
+    }
+
+    public String getLanguage() {
+        AlphabetRepository alphabetRepository = AlphabetRepository.getRepository();
         String langCode = "";
         int mostMatches = 0;
-        for (Map.Entry<String, List<Character>> currentLang : AlphabetRepository.getLangMap().entrySet()) {
-            int containingNumber = 0;
-            for (int bufferedChar : buffer) {
-                if (currentLang.getValue().contains((char) bufferedChar)) {
-                    containingNumber++;
-                }
-            }
-            if (containingNumber == buffer.length) {
+        for (Map.Entry<String, List<Character>> currentLang : alphabetRepository.getLangMap().entrySet()) {
+            int numberOfMatches = countCharContain(currentLang.getValue());
+            if (numberOfMatches == buffer.length) {
                 return currentLang.getKey();
             }
-            if (containingNumber > mostMatches) {
-                mostMatches = containingNumber;
+            if (numberOfMatches > mostMatches) {
+                mostMatches = numberOfMatches;
                 langCode = currentLang.getKey();
             }
         }
         return langCode;
     }
 
-    //TODO: too much for
-    public static String getLanguage(String[] buffer) {
-        String langCode = "";
-        int mostMatches = 0;
-        for (Map.Entry<String, List<Character>> currentLang : AlphabetRepository.getLangMap().entrySet()) {
-            int containingNumber = 0;
-            for (String line : buffer) {
-                for (char character : line.toCharArray()) {
-                    if (currentLang.getValue().contains(character)) {
-                        containingNumber++;
-                    }
+    private int countCharContain(List<Character> currentLang) {
+        int numberOfMatches = 0;
+        for (String line : buffer) {
+            for (char character : line.toCharArray()) {
+                if (currentLang.contains(character)) {
+                    numberOfMatches++;
                 }
             }
-            if (containingNumber == buffer.length) {
-                return currentLang.getKey();
-            }
-            if (containingNumber > mostMatches) {
-                mostMatches = containingNumber;
-                langCode = currentLang.getKey();
-            }
         }
-        return langCode;
+        return numberOfMatches;
     }
 
-    private HashMap<Character, Integer> analyzeCharFrequency(String[] buffer) {
-        String language = TextAnalyzer.getLanguage(buffer);
-        List<Character> alphabet = AlphabetRepository.getAlphabet(language);
+    public Map<Character, Integer> analyzeCharFrequency() {
+        AlphabetRepository alphabetRepository = AlphabetRepository.getRepository();
+        String language = getLanguage();
+        List<Character> alphabet = alphabetRepository.getAlphabetWithSymbols(language);
         HashMap<Character, Integer> charFrequency = new HashMap<>();
         for (char alphabetChar : alphabet) {
-            int freq = 0;
-            for (String line : buffer) {
-                for (char currentChar : line.toCharArray()) {
-                    if (currentChar == alphabetChar) {
-                        freq++;
-                    }
-                }
-            }
+            int freq = countCharMatches(alphabetChar);
             charFrequency.put(alphabetChar, freq);
         }
         return charFrequency;
     }
 
-    public List<Character> getSortedByFreqChars(String[] buffer){
+    private int countCharMatches(char alphabetChar) {
+        int freq = 0;
+        for (String line : buffer) {
+            for (char currentChar : line.toCharArray()) {
+                if (currentChar == alphabetChar) {
+                    freq++;
+                }
+            }
+        }
+        return freq;
+    }
+
+    public List<Character> getSortedByFreqChars() {
         List<Character> sortedChars = new ArrayList<>();
-        HashMap<Character, Integer> charFrequency = analyzeCharFrequency(buffer);
-        List<Map.Entry<Character,Integer>> map= new ArrayList<>(charFrequency.entrySet());
+        HashMap<Character, Integer> charFrequency = new HashMap<>(analyzeCharFrequency());
+        List<Map.Entry<Character, Integer>> map = new ArrayList<>(charFrequency.entrySet());
         map.sort(new Comparator<>() {
             @Override
             public int compare(Map.Entry<Character, Integer> entry1, Map.Entry<Character, Integer> entry2) {
-                return entry2.getValue()-entry1.getValue();
+                return entry2.getValue() - entry1.getValue();
             }
         });
-        for (Map.Entry<Character,Integer> m: map) {
+        for (Map.Entry<Character, Integer> m : map) {
             sortedChars.add(m.getKey());
         }
         return sortedChars;
     }
 }
-
